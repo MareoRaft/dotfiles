fe41d841479337924794668f5e28a7613e290ff0
Matthew Lancellotti <mlancellotti@hubcitymedia.com>
2018-10-23 10:48:45 -0400

    finish safe install

--- install.py
+++ install.py
@@ -24,10 +24,10 @@ def install_safe():
 	if PATH['repo'] == PATH['home']:
 		print('The location of the directory containing `install.py` IS your home folder.  This suggests that dotfiles is ALREADY INSTALLED.  Aborting.')
 	else:
-		response = input('The detected location of the dotfiles repo is {}.  Is this correct?  If you type "y" and hit RETURN, the repo will be DELETED, so please make sure it is correct.'.format(PATH['repo']))
+		response = input('The detected location of the dotfiles repo is {}.  Is this correct?  If you type "y" and hit RETURN, that folder will be DELETED, so please make sure it is correct.'.format(PATH['repo']))
 		if response == 'y':
-			print('removing')
-			# install()
+			install()
+			print('Installation complete.  Go to your home directory.')
 		else:
 			print('You did not enter "y".  Aborting.')
 
