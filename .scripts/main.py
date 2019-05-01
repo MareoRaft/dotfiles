51c85108fa62fc226ec76185da0ed49d39d53703
Matthew Lancellotti <mlancellotti@hubcitymedia.com>
2019-05-01 13:17:13 -0400

    automated /etc/hosts updating now works

--- .scripts/main.py
+++ .scripts/main.py
@@ -5,6 +5,8 @@ import sys
 import shutil
 import argparse
 
+from python_hosts import Hosts
+
 import utils
 
 # establish some paths
@@ -12,6 +14,7 @@ PATH_REPO = path.dirname(path.dirname(path.abspath(__file__)))
 PATH = {
 	'home': path.expanduser('~'),
 	'repo': PATH_REPO,
+	'add-host': path.join(PATH_REPO, '.scripts', 'add-host.sh'),
 }
 
 def install_repo():
@@ -25,9 +28,19 @@ def install_repo():
 def install_hosts():
 	# update /etc/hosts with your custom hosts
 	path_hosts = path.join(PATH['repo'], 'hosts')
-	ip_address = '1'
-	host_name = 'hh'
-	utils.run(['sh', 'add-host.sh', ip_address, host_name])
+	# read hosts
+	hosts = Hosts(path_hosts)
+	# write hosts
+	# note that the python_hosts library is extensive and could be used to write in a smarter way, but use add-host.sh for now
+	for host_entry in hosts.entries:
+		if host_entry.entry_type in ['comment', 'blank']:
+			pass
+		elif len(host_entry.names) > 1:
+			print('Multiple host names not implemented.  (for {}.)  skipping.'.format(host_entry.names))
+		else:
+			ip_address = host_entry.address
+			host_name = host_entry.names[0]
+			utils.run(['sh', PATH['add-host'], ip_address, host_name])
 
 if __name__ == '__main__':
 	print('do not run this file directly.  run install.py instead.')
