d0c3c08927bae3be26f19929fbc43646d56fb536
Matthew Lancellotti <matthew.lancellotti@westcreekfin.com>
2023-04-13 19:31:00 -0400

    feat: add emacs support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,8 @@
-(windmove-default-keybindings)
+
+;; This is great except that I can't get it to work with 'meta (alt). It works great with shift, but then I can't use shift to select multiple lines of code.
+(windmove-default-keybindings 'meta)
+
 ;; (global-set-key (kbd "M-<left>")  'windmove-left) this code is commented it out. we can delete it soon
+
+;; This means that emacs will remember which windows you had open when it was launched from a particular directory. Therefore the directory from which you launch emacs determines the emacs session.
+(desktop-save-mode 1)
