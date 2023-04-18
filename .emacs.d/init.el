6be14777e9adb57878858acd35f3ca72b9ad0278
Matthew Lancellotti <matthew.lancellotti@westcreekfin.com>
2023-04-18 17:00:07 -0400

    enable unmodified buffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,10 +46,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(unmodified-buffer evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Enable Unmodified Buffer
+(require 'unmodified-buffer)
+(add-hook 'after-init-hook 'unmodified-buffer-global-mode)
