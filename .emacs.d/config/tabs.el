bfb45ba6f403bb3e447992de35f088595dcd317f
Matthew Lancellotti <matthew.lancellotti@westcreekfin.com>
2023-05-08 18:38:29 -0400

    now indentation works nicely but not unindent.

--- .emacs.d/config/tabs.el
+++ .emacs.d/config/tabs.el
@@ -1,13 +1,37 @@
 ;; When viewing a tab, it appears to be `tab-width` spaces wide.
-(setq-default tab-width 4)
+;;(setq-default tab-width 4)
 
 ;; In text mode...
-(add-hook 'text-mode-hook
-          '(lambda ()
+;;(add-hook 'text-mode-hook
+;;          '(lambda ()
 	     ;; When inserting tabs, insert spaces instead.
              ;; (setq indent-tabs-mode nil)
 	     ;; When highlighting text and hitting tab, indent the lines.
-             (setq indent-line-function (quote insert-tab))
-))
+             ;; (setq indent-line-function (quote insert-tab))
+;;))
 
-;; If you need nice indenting, unindenting, use C-x TAB followed by left, right, S-left, S-right.
+
+(defcustom tab-shift-width 4
+  "Sets selected text shift width on tab"
+  :type 'integer)
+(make-variable-buffer-local 'tab-shift-width)
+
+(global-set-key
+ (kbd "TAB")
+ (lambda (start end)
+   (interactive "r")
+   (if (use-region-p)
+       (save-excursion
+     (let ((deactivate-mark nil))
+       (indent-rigidly start end tab-shift-width)))
+     (indent-for-tab-command))))
+
+(global-set-key
+ (kbd "S-TAB")
+ (lambda (start end)
+   (interactive "r")
+   (if (use-region-p)
+       (save-excursion
+     (let ((deactivate-mark nil))
+       (indent-rigidly start end (- tab-shift-width))))
+     (indent-for-tab-command))))
