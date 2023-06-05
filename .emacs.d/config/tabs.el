f990dfae8958af36582cb5219e646d7501b375bd
Matthew Lancellotti <matthew.lancellotti@westcreekfin.com>
2023-06-05 15:57:20 -0400

    fix glitch in git pushh.

--- .emacs.d/config/tabs.el
+++ .emacs.d/config/tabs.el
@@ -1,40 +1,58 @@
 ;; When viewing a tab, it appears to be `tab-width` spaces wide.
 (setq-default tab-width 4)
 
-;; In text mode...
-;;(add-hook 'text-mode-hook
-;;          '(lambda ()
-	     ;; When inserting tabs, insert spaces instead.
-             ;; (setq indent-tabs-mode nil)
-	     ;; When highlighting text and hitting tab, indent the lines.
-             ;; (setq indent-line-function (quote insert-tab))
-;;))
+;; Use spaces instead of tabs.
+;; (if user asks to indent to position 7, for example, do not insert a tab followed by 3 spaces, but rather insert all spaces)
+(setq-default indent-tabs-mode nil)
+
+;; ;; TEMP DISABLE THIS so we can try out GUESS STYLE, which is a more complete solution, even though this WORKS
+;; (defun infer-indentation-style ()
+;;   ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
+;;   ;; neither, we use the current indent-tabs-mode                               
+;;   (let ((space-count (how-many "^  " (point-min) (point-max)))
+;;         (tab-count (how-many "^\t" (point-min) (point-max))))
+;;     (if (> space-count tab-count) (setq indent-tabs-mode nil))
+;;     (if (> tab-count space-count) (setq indent-tabs-mode t))))
+;; ;; Infer whether to use tabs or spaces based on what the file already uses
+;; (infer-indentation-style)
 
 
-(defcustom tab-shift-width 4
-  "Sets selected text shift width on tab"
-  :type 'integer)
-(make-variable-buffer-local 'tab-shift-width)
 
-;;(require 'general)
-
-(global-set-key
- (kbd "<tab>")
- (lambda (start end)
-   (interactive "r")
-   (if (use-region-p)
-       (save-excursion
-     (let ((deactivate-mark nil))
-       (indent-rigidly start end tab-shift-width)))
-     (indent-for-tab-command))))
+;; ;; In text mode...
+;; (add-hook 'text-mode-hook
+;;     '(lambda ()
+;; 	    ;; When highlighting text and hitting tab, indent the lines.
+;;         (setq indent-line-function (quote insert-tab))
+;; 		)
+;; )
 
 
-(global-set-key
- (kbd "<backtab>")
- (lambda (start end)
-   (interactive "r")
-   (if (use-region-p)
-       (save-excursion
-     (let ((deactivate-mark nil))
-       (indent-rigidly start end (- tab-shift-width))))
-     (indent-for-tab-command))))
+;; (defcustom tab-shift-width 4
+;;   "Sets selected text shift width on tab"
+;;   :type 'integer)
+;; (make-variable-buffer-local 'tab-shift-width)
+
+;; ;;(require 'general)
+
+;; THIS DOES NOT WORK.  DELETE IT SOON.
+;; (global-set-key
+;;  (kbd "<tab>")
+;;  (lambda (start end)
+;;    (interactive "r")
+;;    (if (use-region-p)
+;;        (save-excursion
+;;      (let ((deactivate-mark nil))
+;;        (indent-rigidly start end tab-shift-width)))
+;;      (indent-for-tab-command))))
+
+;; ;; TEMP DISABLE THIS so we can try out GUESS STYLE, which is a more complete solution, even though this WORKS
+;; ;; THIS WORKS. LEAVE IT BE unless you find a tool that replaces both this and <tab> in one go.
+;; (global-set-key
+;;  (kbd "<backtab>")
+;;  (lambda (start end)
+;;    (interactive "r")
+;;    (if (use-region-p)
+;;        (save-excursion
+;;      (let ((deactivate-mark nil))
+;;        (indent-rigidly start end (- 4))))
+;;      (indent-for-tab-command))))
