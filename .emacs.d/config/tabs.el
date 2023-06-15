f9f532f17f84a68b3ec82107505cd86398a953ab
Matthew Lancellotti <matthew.lancellotti@westcreekfin.com>
2023-06-15 14:18:00 -0400

    remove guess-style

--- .emacs.d/config/tabs.el
+++ .emacs.d/config/tabs.el
@@ -1,31 +1,29 @@
 ;; When viewing a tab, it appears to be `tab-width` spaces wide.
 (setq-default tab-width 4)
 
-;; ;; TEMP DISABLE THIS so we can try out GUESS STYLE, which is a more complete solution, even though this WORKS
-;; Use spaces instead of tabs.
-;; (if user asks to indent to position 7, for example, do not insert a tab followed by 3 spaces, but rather insert all spaces)
-;; (setq-default indent-tabs-mode nil)
+;; Use spaces instead of tabs (by DEFAULT).
+;; if user asks to indent to position 7, for example, do not insert a tab followed by 3 spaces, but rather insert all spaces
+(setq-default indent-tabs-mode nil)
 
-;; ;; TEMP DISABLE THIS so we can try out GUESS STYLE, which is a more complete solution, even though this WORKS
-;; (defun infer-indentation-style ()
-;;   ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
-;;   ;; neither, we use the current indent-tabs-mode                               
-;;   (let ((space-count (how-many "^  " (point-min) (point-max)))
-;;         (tab-count (how-many "^\t" (point-min) (point-max))))
-;;     (if (> space-count tab-count) (setq indent-tabs-mode nil))
-;;     (if (> tab-count space-count) (setq indent-tabs-mode t))))
-;; ;; Infer whether to use tabs or spaces based on what the file already uses
-;; (infer-indentation-style)
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
+  ;; neither, we use the current indent-tabs-mode                               
+  (let ((space-count (how-many "^  " (point-min) (point-max)))
+        (tab-count (how-many "^\t" (point-min) (point-max))))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+;; Infer whether to use tabs or spaces based on what the file already uses
+(infer-indentation-style)
 
 
 
-;; ;; In text mode...
-;; (add-hook 'text-mode-hook
-;;     '(lambda ()
-;; 	    ;; When highlighting text and hitting tab, indent the lines.
-;;         (setq indent-line-function (quote insert-tab))
-;; 		)
-;; )
+;; In text mode...
+(add-hook 'text-mode-hook
+    '(lambda ()
+	    ;; When highlighting text and hitting tab, indent the lines.
+        (setq indent-line-function (quote insert-tab))
+		)
+)
 
 
 (defcustom tab-shift-width 4
